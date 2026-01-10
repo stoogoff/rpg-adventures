75622284803684fa442a467e46744bdea24297fd
Stoo <stoo.goff@gmail.com>
2026-01-10 20:52:09 +0000

    Type checking

--- src/modules/core/services/search.ts
+++ src/modules/core/services/search.ts
@@ -5,7 +5,7 @@ import { LinkModel } from '../models/index.ts'
 export class SearchService {
 	constructor(private repo: Repository) {}
 
-	async search(term: string): Promise<LinkModel> {
+	async search(term: string): Promise<LinkModel[]> {
 		const results = await Promise.all([
 			this.repo.getAllByType('adventure'),
 			this.repo.getAllByType('campaign'),
