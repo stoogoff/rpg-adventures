4089187544201c27a9bb14749bcc04713a2713cf
Stoo <stoo.goff@gmail.com>
2026-02-07 17:31:52 +0000

    Added sliding menu for small devices

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -6,4 +6,17 @@ import { searchResults } from './store.js'
 directives.registerStore('searchResults', searchResults)
 
 directives.registerComponent('search', Search)
-directives.load(document.getElementById('site-header'), {})
+directives.load(document.getElementById('site-header'), {
+	openMenu() {
+		const menu = document.getElementById('primary')
+
+		if(menu.classList.contains('active')) {
+			menu.classList.remove('active')
+			menu.classList.add('inactive')
+		}
+		else {
+			menu.classList.remove('inactive')
+			menu.classList.add('active')
+		}
+	},
+})
